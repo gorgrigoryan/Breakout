40f68c407e9ff7bab6e0c2a81482fe6286956330
unknown <gorgrigoryan1995@gmail.com>
2021-09-18 19:18:43 +0400

    initial commit

---
+++ paddle.h
@@ -0,0 +1,22 @@
+#ifndef PADDLE_H
+#define PADDLE_H
+
+class Paddle {
+public:
+    Paddle();
+    ~Paddle();
+
+    void resetState();
+    void move();
+    void setDx(int);
+    QRect getRect();
+    QImage& getImage();
+private:
+    QImage image;
+    QRect rect;
+    int dx;
+    static const int INITIAL_X = 200;
+    static const int INITIAL_Y = 360;
+};
+
+#endif // PADDLE_H
